6f7ba96abe9da052df2114fab281eee67f0ef86a
Keiichi SHIMA <keiichi@iijlab.net>
2014-06-13 12:50:11 +0900

    final commit.  we've merged ukai2 to ukai.

---
+++ README.md
@@ -0,0 +1,2 @@
+All the functions developped under this project is merged to
+the main UKAI project (https://github.com/keiichishima/ukai/)
---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ukai',
+      version='0.3',
+      description='Location Aware Distributed Storage for Virtual Machine Disks',
+      author='Keiichi SHIMA',
+      author_email='keiichi@iijlab.net',
+      url='http://github.com/keiichishima/ukai2/',
+      packages=['libukai'],
+      requires=['fusepy (>=2.0.2)', 'netifaces (>=0.6)', 'riak (>=2.0.3)'],
+      scripts=['scripts/ukai_server',
+               'scripts/ukai_fuse',
+               'scripts/ukai_admin',
+               ],
+      data_files=[('share/doc/ukai', ['ukai_logo.png'])],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Filesystems',
+        ],
+)
+
